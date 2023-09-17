ef63c292c08880b671ed293c24300a93c91232a3
Bhushan G Y <bhushan.benz@gmail.com>
2023-09-17 20:05:40 +0530

    removed script that was preventing form submission

--- main.js
+++ main.js
@@ -11,14 +11,17 @@ file and then js script is executed and then the color changes if preference is 
 document.querySelector("#toggle-theme").addEventListener("click", () => {
   document.documentElement.classList.toggle("light-theme");
 });
+
 const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
 if (prefersLight) {
   console.log("yes");
   document.documentElement.classList.add("light-theme");
 }
+
 document.querySelector("#scroll-down").addEventListener("click", () => {
   window.scrollTo({ top: document.querySelector("#about-me").offsetTop - 20 });
 });
-document.querySelector("#hire-me-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-});
+
+// document.querySelector("#hire-me-form").addEventListener("submit", (e) => {
+//   e.preventDefault();
+// });
